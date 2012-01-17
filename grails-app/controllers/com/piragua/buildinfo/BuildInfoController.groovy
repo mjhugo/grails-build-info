b39cac35b3f347c039f56986a7773cd97b2b922f
Mike Hugo <mike@piragua.com>
2012-01-17 20:37:39 +0000

    add git_commit env variable

--- grails-app/controllers/com/piragua/buildinfo/BuildInfoController.groovy
+++ grails-app/controllers/com/piragua/buildinfo/BuildInfoController.groovy
@@ -7,7 +7,7 @@ import org.codehaus.groovy.grails.plugins.metadata.GrailsPlugin
 @GrailsPlugin(name='build-info', version='1.2')
 class BuildInfoController {
  
-    static final List buildInfoProperties = ['build.date', 'scm.version', 'environment.BUILD_NUMBER', 'environment.BUILD_ID', 'environment.BUILD_TAG', 'environment.GIT_BRANCH'] 
+    static final List buildInfoProperties = ['build.date', 'scm.version', 'environment.BUILD_NUMBER', 'environment.BUILD_ID', 'environment.BUILD_TAG', 'environment.GIT_BRANCH', 'environment.GIT_COMMIT'] 
 
     def index = { 
         def buildInfoConfig = ConfigurationHolder.config?.buildInfo
