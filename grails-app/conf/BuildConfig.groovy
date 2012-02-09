84a9a4f109688e055efd1bd2a33e0f30756fd943
Mike Hugo <mike@entagen.com>
2012-02-09 07:18:34 -0600

    upgrade to grails 2.0

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,50 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.release.scm.enabled = false
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenCentral()
+
+        // uncomment these to enable remote dependency resolution from public Maven repositories
+        //mavenCentral()
+        //mavenLocal()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:1.0.1", ":svn:1.0.2") {
+            export = false
+        }
+
+		test (":code-coverage:1.2.5") {
+			export = false
+		}
+
+    }
+}
