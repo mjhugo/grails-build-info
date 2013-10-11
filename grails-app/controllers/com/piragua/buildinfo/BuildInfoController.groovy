b00d0d19d6107bdf6058994eb53a88fb41671df2
Ronny Løvtangen <ronny@lovtangen.com>
2013-10-11 11:25:29 +0200

    Annotation GrailsPlugin is not needed. Added by build. Asked Graeme Rocher about this at GR8Conf EU.

--- grails-app/controllers/com/piragua/buildinfo/BuildInfoController.groovy
+++ grails-app/controllers/com/piragua/buildinfo/BuildInfoController.groovy
@@ -1,10 +1,8 @@
 package com.piragua.buildinfo
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
-import org.codehaus.groovy.grails.plugins.metadata.GrailsPlugin
 
 
-@GrailsPlugin(name='build-info', version='1.2')
 class BuildInfoController {
  
     static final List buildInfoProperties = ['build.date', 'scm.version', 'environment.BUILD_NUMBER', 'environment.BUILD_ID', 'environment.BUILD_TAG', 'environment.GIT_BRANCH', 'environment.GIT_COMMIT'] 
