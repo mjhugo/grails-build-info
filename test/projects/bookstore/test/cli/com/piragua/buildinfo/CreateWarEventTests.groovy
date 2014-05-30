4af8ac2a14aef6889c8b4f3e98ef60c97568dcff
Ronny Løvtangen <ronny@lovtangen.com>
2014-05-31 00:50:14 +0200

    use power asserts for better error messages

--- test/projects/bookstore/test/cli/com/piragua/buildinfo/CreateWarEventTests.groovy
+++ test/projects/bookstore/test/cli/com/piragua/buildinfo/CreateWarEventTests.groovy
@@ -10,17 +10,17 @@ class CreateWarEventTests extends AbstractCliTestCase {
     void testCreateWar(){
         execute (['war', '-non-interactive'])
 
-        assertEquals 0, waitForProcess()
+        assert waitForProcess() == 0
 
         verifyHeader()
 
         Properties props = new Properties()
         props.load(new ZipFile('target/bookstore-0.1.war').getInputStream(new ZipEntry('WEB-INF/classes/application.properties')))
 
-        assertNotNull props['scm.version']
-        assertNotNull props['build.date']
+        assert props['scm.version']
+        assert props['build.date']
 
-        assertEquals 'hugo', props['who.was.here']
+        assert props['who.was.here'] == 'hugo'
 
     }
 
