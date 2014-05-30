3baaf9dc006a063b6dedbaccac503ff11708103e
Ronny Løvtangen <ronny@lovtangen.com>
2014-05-31 00:23:41 +0200

    Upgrade test project to Grails 2.1.5 to prepare for Grails 2.4 support. To add Grails 2.4 support we need to replace ConfigurationHolder with Holders, thus the test project needs to be at least Grails 2.0. Had some issues with inline plugins with 2.2 and 2.3, so choosing the latest 2.1.x This is a standard Grails 2.1.5 project with changes only to: - grails-app/conf/BuildConfig.groovy (build-info inline plugin, functional-test plugin) - grails-app/i18n/messages.properties (property 'app.version') - scripts/_Events.groovy (new file) - test/cli (new directory) - test/functional (new directory)

--- test/projects/bookstore/grails-app/conf/BuildConfig.groovy
+++ test/projects/bookstore/grails-app/conf/BuildConfig.groovy
@@ -1,24 +1,32 @@
-grails.plugin.location."build-info" = "../../.."
-
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits( "global" ) {
-        // uncomment to disable ehcache
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {        
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised but here for backwards compatibility
+
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
+        grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
+        mavenLocal()
+        mavenCentral()
+
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -27,7 +35,26 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        // runtime 'mysql:mysql-connector-java:5.1.22'
     }
 
+    plugins {
+        runtime ":hibernate:$grailsVersion"
+        runtime ":jquery:1.8.3"
+        runtime ":resources:1.1.6"
+
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.5"
+
+        build ":tomcat:$grailsVersion"
+
+        runtime ":database-migration:1.3.2"
+
+        compile ':cache:1.0.1'
+        compile ":functional-test:2.0.0"
+    }
 }
+
+grails.plugin.location."build-info" = "../../.."
