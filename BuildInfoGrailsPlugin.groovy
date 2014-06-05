725d4c644fc0bddfb34fb381870819bd233d824c
Ronny Løvtangen <ronny@lovtangen.com>
2014-06-05 15:43:42 +0200

    Set next snapshot version

--- BuildInfoGrailsPlugin.groovy
+++ BuildInfoGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.codehaus.groovy.grails.commons.ServiceArtefactHandler
 
 class BuildInfoGrailsPlugin {
     // the plugin version
-    def version = "1.2.6"
+    def version = "1.2.7-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
