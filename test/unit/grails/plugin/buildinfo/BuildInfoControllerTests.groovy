1712c524f893829149fdde26458c8e15352dd477
Eric Helgeson <erichelgeson@gmail.com>
2014-12-03 10:19:33 -0600

    Add xml/json endpoints to buildInfo

--- test/unit/grails/plugin/buildinfo/BuildInfoControllerTests.groovy
+++ test/unit/grails/plugin/buildinfo/BuildInfoControllerTests.groovy
@@ -3,29 +3,29 @@ package grails.plugin.buildinfo
 import grails.test.ControllerUnitTestCase
 
 class BuildInfoControllerTests extends ControllerUnitTestCase {
+    def grailsApplication = new org.codehaus.groovy.grails.commons.DefaultGrailsApplication()
 
     void testIndex(){
+        controller.grailsApplication = grailsApplication
         controller.index()
         assertEquals 'index', renderArgs.view
 
         assertEquals controller.buildInfoProperties, renderArgs.model.buildInfoProperties
     }
 
-
     void testIndex_overrideDefaults(){
         mockConfig """
             buildInfo.properties.exclude = ['scm.version']
             buildInfo.properties.add = ['custom.property']
         """
-
+        controller.grailsApplication = grailsApplication
         controller.index()
         assertEquals 'index', renderArgs.view
 
-        def expectedProperties = controller.buildInfoProperties - 'scm.version' 
+        def expectedProperties = controller.buildInfoProperties - 'scm.version'
         expectedProperties = expectedProperties + 'custom.property'
 
         assertEquals expectedProperties, renderArgs.model.buildInfoProperties
     }
-
 
 }
